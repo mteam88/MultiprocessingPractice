21e62367ef1bfae9bf2a703153c23f3981a947cb
Replit user <>
2022-09-08 17:57:24 +0000

    sqrt task done

--- main.py
+++ main.py
@@ -8,17 +8,22 @@ Task: Print the square root of every element in a list.
 """
 INPUT_LIST = list(range(40))
 
-def task_printsqrt(elem):
-	print(math.sqrt(elem))
+class Task:
+	def exec(self, elem):
+		raise Exception
+
+class Task_PrintSqrt(Task):
+	def exec(self, num):
+		print(math.sqrt(num))
 
 def worker(inpt, task):
 	while True:
-		task(inpt.get(True))
+		task.exec(inpt.get(True))
 		inpt.task_done()
 
 inpt = mp.JoinableQueue()
 for num in INPUT_LIST:
 	inpt.put(num)
 
-pool = mp.Pool(4, worker, (inpt, task_printsqrt))
+pool = mp.Pool(4, worker, (inpt, Task_PrintSqrt()))
 inpt.join()
